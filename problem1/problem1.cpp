45a25dab1200a3984a6fb71e328f22850c97b830
Josh Tighe <joshdtighe@gmail.com>
2013-10-26 18:57:43 -0400

    Adding new directory, hello world program, and makefile.

---
+++ problem1/problem1.cpp
@@ -0,0 +1,14 @@
+
+/*
+ * Project Euler - Problem 1
+ * 
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ */
+
+#include <iostream>
+
+int main () {
+
+  std::cout << "Hey world!\n";
+}
