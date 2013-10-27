0bbba8273e33f9a3be56249e8975109b366daf71
Josh Tighe <joshdtighe@gmail.com>
2013-10-26 20:18:56 -0400

    Initial solution to Problem 1 - Sum of multiples of 3 and 5.

--- problem1/problem1.cpp
+++ problem1/problem1.cpp
@@ -10,5 +10,16 @@
 
 int main () {
 
-  std::cout << "Hey world!\n";
+  const int UPPER_BOUND = 1000;
+  int sumOfMultiples = 0;
+
+  for (int i = 1; i < UPPER_BOUND; ++i) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sumOfMultiples += i;
+    }
+  }
+
+  std::cout << "The sum of the multiples of 3 and 5 below " << UPPER_BOUND << " is " << sumOfMultiples << "\n";
+
+  return 0;
 }
